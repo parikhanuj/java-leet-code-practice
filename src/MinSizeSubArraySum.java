6615f39cadcdf77d7bac550d6b9f01c4bffa203e
Anuj Parikh <anujparikh1@gmail.com>
2024-12-03 23:29:23 -0600

    Minimum Size Subarray Sum

--- src/MinSizeSubArraySum.java
+++ src/MinSizeSubArraySum.java
@@ -6,22 +6,15 @@ public class MinSizeSubArraySum {
         int p1 = 0, p2 = 0, currentSubArrayLength = 0, currentWindowSum = 0;
 
         while (p2 < nums.length) {
-            if (currentWindowSum < target) {
-                currentWindowSum += nums[p2];
-                p2++;
-                currentSubArrayLength++;
-            } else {
+            currentWindowSum += nums[p2];
+            p2++;
+            currentSubArrayLength++;
+            while (currentWindowSum >= target) {
                 minSubArrayLength = Math.min(currentSubArrayLength, minSubArrayLength);
                 currentWindowSum -= nums[p1];
                 p1++;
                 currentSubArrayLength--;
             }
-        }
-        while (currentWindowSum >= target && p1 < nums.length) {
-            minSubArrayLength = Math.min(currentSubArrayLength, minSubArrayLength);
-            currentWindowSum -= nums[p1];
-            p1++;
-            currentSubArrayLength--;
         }
 
         return minSubArrayLength == nums.length + 1 ? 0 : minSubArrayLength;
--- test/MinSizeSubArraySumTest.java
+++ test/MinSizeSubArraySumTest.java
@@ -2,7 +2,6 @@
 
 import org.junit.Assert;
 import org.junit.Test;
-import java.util.Arrays;
 
 public class MinSizeSubArraySumTest {
 
