d3f43e2605bc2019244f246c43ab64e6c570305e
Anuj Parikh <anujparikh1@gmail.com>
2024-12-15 11:07:20 -0600

    Longest Consecutive Sequence

---
+++ src/LongestConsecutiveSeq.java
@@ -0,0 +1,30 @@
+class LongestConsecutiveSeq {
+    public int longestConsecutive(int[] nums) {
+        Map<Integer, Boolean> presentAndConsiderMap = new HashMap<>();
+        int maxLength = 0;
+        for (int i = 0; i < nums.length; i++) {
+            presentAndConsiderMap.put(nums[i], false);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (!presentAndConsiderMap.get(nums[i])) {
+                int postiveSeq = nums[i];
+                int negativeSeq = nums[i] - 1;
+                int currentPositiveLength = 0;
+                int currentNegativeLength = 0;
+
+                while (presentAndConsiderMap.containsKey(postiveSeq)) {
+                    presentAndConsiderMap.put(postiveSeq, true);
+                    currentPositiveLength++;
+                    postiveSeq++;
+                }
+                while (presentAndConsiderMap.containsKey(negativeSeq)) {
+                    presentAndConsiderMap.put(negativeSeq, true);
+                    currentNegativeLength++;
+                    negativeSeq--;
+                }
+                maxLength = Math.max(maxLength, currentPositiveLength + currentNegativeLength);
+            }
+        }
+        return maxLength;
+    }
+}
