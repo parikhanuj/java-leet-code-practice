02603e49f4207ec3fdd8cd0b5655c2d1e33f74b5
Anuj Parikh <anujparikh1@gmail.com>
2024-12-17 15:17:09 -0600

    Minimum Window Sub String

--- src/MinWindowSubString.java
+++ src/MinWindowSubString.java
@@ -1,47 +1,42 @@
 public class MinWindowSubString {
-    int[] tOccMap = new int[256];
-    int[] sOccMap = new int[256];
 
-    private boolean isSubString(String comStr) {
-        for (int i = 0; i < comStr.length(); i++) {
-            if (tOccMap[comStr.charAt(i) - 'A'] > sOccMap[comStr.charAt(i) - 'A']) {
-                return false;
-            }
-        }
-        return true;
-    }
+    private Map<Character, Integer> tCharMap = new HashMap<>();
+    private Map<Character, Integer> sCharMap = new HashMap<>();
 
     public String minWindow(String s, String t) {
-        String minWindow = s;
-        int start = 0;
-        boolean foundWindow = false;
 
-        if (s.length() < t.length()) {
-            return "";
+    if (s.length() < t.length()) return "";
+
+    boolean isSubStringPresent = false;
+    String result = s;
+
+    for (int i = 0; i < t.length(); i++) {
+        char charAtI = t.charAt(i);
+        tCharMap.put(charAtI, tCharMap.getOrDefault(charAtI, 0) + 1);
+    }
+
+    int left = 0;
+    int right = 0;
+    while (right < s.length()) {
+        char charAtRight = s.charAt(right);
+        sCharMap.put(charAtRight, sCharMap.getOrDefault(charAtRight, 0) + 1);
+        while (isSubStringPresent()) {
+        isSubStringPresent = true;
+        result = result.length() > s.substring(left, right + 1).length() ? s.substring(left, right + 1) : result;
+        char charAtLeft = s.charAt(left);
+        sCharMap.put(charAtLeft, sCharMap.get(charAtLeft) - 1);
+        left++;
         }
+        right++;
+    }
 
-        for (int i = 0; i < t.length(); i++) {
-            tOccMap[t.charAt(i) - 'A']++;
-            sOccMap[s.charAt(i) - 'A']++;
-        }
+    return isSubStringPresent ? result : "";
+    }
 
-        for (int end = t.length(); end <= s.length(); end++) {
-            if(isSubString(t)) {
-                String matchStr = s.substring(start, end);
-                minWindow = matchStr.length() < minWindow.length() ? matchStr : minWindow;
-                sOccMap[s.charAt(start) - 'A']--;
-                start++;
-                foundWindow = true;
-            }
-            if (end < s.length()) {
-                sOccMap[s.charAt(end) - 'A']++;
-                while (start <= end && sOccMap[s.charAt(start) - 'A'] > tOccMap[s.charAt(start) - 'A']) {
-                    sOccMap[s.charAt(start) - 'A']--;
-                    start++;
-                }
-            }
-        }
-
-        return foundWindow ? minWindow : "";
+    private boolean isSubStringPresent() {
+    if (!sCharMap.keySet().containsAll(tCharMap.keySet())) return false;
+    List<Character> misMatchChar = tCharMap.keySet().stream().filter(key ->
+        sCharMap.containsKey(key) && tCharMap.get(key) > sCharMap.get(key)).collect(Collectors.toList());
+    return misMatchChar.isEmpty();
     }
 }
