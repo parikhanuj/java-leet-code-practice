a17dce04f3ab04abba5c365288bb343c8aa17950
Anuj Parikh <anujparikh1@gmail.com>
2024-12-19 11:04:03 -0600

    Daily Temperatures

---
+++ src/DailyTemperatures.java
@@ -0,0 +1,19 @@
+class DailyTemperatures {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<int[]> stack = new Stack<>();
+        int[] result = new int[temperatures.length];
+
+        for (int i = 0; i < temperatures.length; i++) {
+            if (stack.size() == 0) {
+                stack.add(new int[]{temperatures[i], i});
+            } else {
+                while (!stack.isEmpty() && stack.peek()[0] < temperatures[i]) {
+                    int[] top = stack.pop();
+                    result[top[1]] = i - top[1];
+                }
+                stack.add(new int[]{temperatures[i], i});
+            }
+        }
+        return result;
+    }
+}
