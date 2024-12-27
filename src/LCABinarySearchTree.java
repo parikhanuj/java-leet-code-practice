81dba46220c44fcd0232d0e3f9dc2644eabc3a32
Anuj Parikh <anujparikh1@gmail.com>
2024-12-26 18:51:05 -0600

    Lowest Common Ancestor of a Binary Search Tree

---
+++ src/LCABinarySearchTree.java
@@ -0,0 +1,11 @@
+public class LCABinarySearchTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+        if (p.val <= root.val && q.val >= root.val) return root;
+        if (q.val <= root.val && p.val >= root.val) return root;
+        if (p.val <= root.val && q.val <= root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        return lowestCommonAncestor(root.right, p, q);
+    }
+}
