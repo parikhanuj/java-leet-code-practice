758a839ebbe2b0e87329791570205b8387341568
Anuj Parikh <anujparikh1@gmail.com>
2024-12-29 14:04:02 -0600

    Notes for min stack class design

--- notes/class-design.md
+++ notes/class-design.md
@@ -13,3 +13,11 @@
 - For decoding
   - in the input string we would have length of each word before the word itself
     - like for `005#hello005#world004#test` first word would be 5 letters and it would be `hello`
+
+## Min Stack
+- [Neetcode Example](https://neetcode.io/problems/minimum-stack)
+- Use 2 stacks, one to store actual values and one to store min values at that point of time
+- `push()` - push actual number to first stack and minimum of `minStack.peek()` and `value` to top of minStack
+- `pop()` - pop from both the stacks
+- `top()` - peek from first stack
+- `getMin()` - peek from minStack stack
---
+++ src/SlidingWindowMax2Deque.java
@@ -0,0 +1,42 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class SlidingWindowMax2Deque {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int left = 0;
+        int right = 0;
+        int n = nums.length;
+        Deque<Integer> q = new LinkedList<>();
+        int[] result = new int[nums.length - k + 1];
+
+        while (right < n) {
+
+            while(!q.isEmpty() && nums[q.getFirst()] < nums[q.getLast()]) {
+                q.removeFirst();
+            }
+
+            if (!q.isEmpty() && nums[q.getLast()] < nums[right]) {
+                q.removeLast();
+            }
+            q.addLast(right);
+
+            if (right >= k) {
+                result[left] = nums[q.getFirst()];
+                left++;
+
+                if (q.getFirst() < left) {
+                    q.removeFirst();
+                }
+            }
+
+            right++;
+        }
+
+        while(!q.isEmpty() && nums[q.getFirst()] < nums[q.getLast()]) {
+            q.removeFirst();
+        }
+        result[left] = nums[q.getFirst()];
+
+        return result;
+    }
+}
